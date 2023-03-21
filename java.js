f4af795301316c9485a53751ef35ff2784eb1b83
imtiajlinux <imtiaj.linux@gmail.com>
2023-03-21 17:31:34 +0600

    hhhh

--- java.js
+++ java.js
@@ -1,13 +1,6 @@
-const people = [
-    {name:'bob' , age:23, position:'developer'},
-    {name:'ibrahim' , age:54, position:'manager'},
-    {name:'adam' , age:43, position:'pro'},
-    {name:'foysal' , age:25, position:'boss'},
-    {name:'robart' , age:22, position:'juner developer'},
-    {name:'json' , age:33, position:'inten'}
-]
+const btn = document.querySelector('.btn')
 
-const afe= people.filter((tot)=> {
-     return tot.age<25
+
+btn.addEventListener('click', ()=>{
+    console.log('clicked it');
 })
-console.log(afe);
